c1f3f2e5f5536b55539ea53cc46ad62a801df741
Michael Hamilton <michaelhamilton626@gmail.com>
2016-04-14 14:14:55 -0400

    Added route and model for prezis
    Added adapter that connects to backend service and loads data to model.
     Basic view outputs all prezis

---
+++ app/models/prezi.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  thumbnail: DS.attr('string'),
+  creator: DS.attr(),
+  createdAt: DS.attr('string')
+});
