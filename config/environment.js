ee7a877c3cd5ab59003931f3e44cf487ea485236
Michael Hamilton <michaelhamilton626@gmail.com>
2016-04-15 00:21:02 -0400

    Updated backend host
    Changed backend hostname, updating environment

--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      host: 'https://peaceful-river-56443.herokuapp.com'
+      host: 'https://mhamilton-prezi-backend.herokuapp.com'
     }
   };
 
